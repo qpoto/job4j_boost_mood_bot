ea5bbe089a62e20b113ff244f737fb4027c56272
StanislavInform <qpoto@yandex.ru>
2024-12-03 12:51:09 +0300

    Жизненный цикл Spring Bean [#505292] Lj, Добавлена информация об инициализации объекта и его уничтожение через аннотации @PreConstruct и @PostDestroy для классов: TelegrammBotService, BotCommandHandler,  AchievementService, RecommendationEngine, MoodService, RemindService

--- src/main/java/ru/job4j/bmb/services/TelegramBotService.java
+++ src/main/java/ru/job4j/bmb/services/TelegramBotService.java
@@ -1,7 +1,11 @@
 package ru.job4j.bmb.services;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Service;
 import ru.job4j.bmb.content.Content;
 
+ @Service
 public class TelegramBotService {
     private final BotCommandHandler handler;
 
@@ -12,4 +16,14 @@ public class TelegramBotService {
     public void receive(Content content) {
         handler.receive(content);
     }
+
+     @PostConstruct
+     public void init() {
+         System.out.println("Bean is going through @PostConstruct init.");
+     }
+
+     @PreDestroy
+     public void destroy() {
+         System.out.println("Bean will be destroyed via @PreDestroy.");
+     }
 }
